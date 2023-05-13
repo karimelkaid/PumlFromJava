05b6e1b9893cea0675e7a6e0c9aa4761a145c28e
kelkaid <elkarim741@gmail.com>
2023-05-13 17:13:41 +0200

    Fin du code semaine 3 (question à poser au prof dans Java2puml dernière ligne)

--- src/pumlFromJava/Java2Puml.java
+++ src/pumlFromJava/Java2Puml.java
@@ -5,13 +5,16 @@ import java.util.spi.ToolProvider;
 
 
 /*
-    - Modifier le DCA (et DCC du coup), rajouter Doclet.Option, Tree, Option.Kind, etc... au max
+    - Modifier le DCA (et DCC du coup), rajouter Doclet.Option, DocTree, Option.Kind, etc... --> faut faire tout ce qu'on aura besoin dans ce projet même si c'est pas dans cette semaine --> FAIT
+    - Dans le DCA et DCC, jsp comment relier la classe DocTrees avec le reste
     - Dans le DS :
         - Modifier MyDoclet.class par Java2Puml.class
         - Ajouter un objet DocletEnvironnement pck on en a besoin quand j'appelle run(environnement)
         - Le point d'entrée c'est Java2Puml après faut faire le DS de Java2Puml et ENSUITE quand on a appelé le run on recopie ce qu'il y a déjà
-    - Dans le code :
-        - Il faut ajouter une classe PumlDiagram (voir dans la dernière question) et faire une partie du code dedans
+    - Ajouter le package western dans la semaine 2
+
+    - À demander :
+        - Est ce qu'on doit gérer le cas où on a un package contenant DES packages ? (par exemple dans western on a un package Scénario 1 etc...) Perso j'ai géré uniquement le cas où y a UN SEUL package
 
 */
 public class Java2Puml
--- src/pumlFromJava/PumlDiagram.java
+++ src/pumlFromJava/PumlDiagram.java
@@ -1,19 +1,29 @@
 package pumlFromJava;
 
+import com.sun.source.util.DocTrees;
 import jdk.javadoc.doclet.DocletEnvironment;
 
 import javax.lang.model.element.Element;
 import javax.lang.model.element.ElementKind;
+import javax.lang.model.element.TypeElement;
+import javax.lang.model.element.VariableElement;
+import javax.lang.model.type.DeclaredType;
+import javax.lang.model.type.PrimitiveType;
+import javax.lang.model.type.TypeKind;
+import javax.lang.model.type.TypeMirror;
 import java.io.FileWriter;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Set;
 
+import static java.util.Arrays.asList;
+
 public class PumlDiagram
 {
-    String repertoireDestination;
-    String nomFichierACree;
-    DocletEnvironment environment;
+    private String repertoireDestination;
+    private String nomFichierACree;
+    private DocletEnvironment environment;
 
     public PumlDiagram( String repertoireDestination, String nomFichierACree, DocletEnvironment environment )
     {
@@ -55,12 +65,12 @@ public class PumlDiagram
 
     }
 
-    public List<Element> recupClasses(DocletEnvironment Xenvironment)
+    public List<Element> recupClasses(DocletEnvironment environment)
     {
         List<Element> res = new ArrayList<>();
 
         // Récupération du/des élément(s) spécifié(s)
-        Set<? extends Element> specifiedElements = Xenvironment.getSpecifiedElements();
+        Set<? extends Element> specifiedElements = environment.getSpecifiedElements();
 
         // Parcourt du/des élément(s) spécifié(s)
         for (Element element : specifiedElements)
@@ -84,14 +94,17 @@ public class PumlDiagram
     {
         StringBuilder res = new StringBuilder("@startuml\n\n");
 
+        res = ajoutePackage(res,classes);
         for( Element classe : classes )
         {
             res = ajouteTypeElement(res, classe);
             res = ajouteChamps(res, classe);
-            res.append("}\n\n");
+            res.append("}\n");
 
+            res = ajouteAgregations(res, classe);
+            res.append("\n\n");
         }
-        res.append("\n@enduml\n");
+        res.append("}\n@enduml\n");
 
         return res.toString();
     }
@@ -155,15 +168,47 @@ public class PumlDiagram
 
         if( stereotype.equals("") )     // S'il n'y a aucun stéréotype --> c'est une classe normale
         {
-            res.append( "class "+classe.getSimpleName().toString()+"{\n" );
+            res.append( "class "+classe.getSimpleName().toString() );
+
+            //  Récupération de la super classe
+            String nomClasse = getSuperClassName(classe);
+            if( nomClasse != null && !nomClasse.equals("Object") )    // Si la classe possède une super classe ET ce n'est pas la classe Object--> ajout de l'héritage
+            {
+                res.append(" extends "+nomClasse);
+            }
+
+            // Ajout des interfaces implémentées
+            List<String> nomInterfaces = getInterfaces(classe);
+            if( nomInterfaces.size() != 0 )
+            {
+                res.append( " implements "+nomInterfaces.get(0) );
+
+                if( (long) nomInterfaces.size() > 1 )
+                {
+                    for(int i = 1; i< nomInterfaces.size(); i++)
+                    {
+                        String nomInterface = nomInterfaces.get(i);
+                        res.append(", "+nomInterface);
+                    }
+                }
+            }
+
+            res.append("{\n");
+
         }
-        else    // Sinon, l'élément est soit une énumération, soit une interface
+        else if(stereotype.equals("interface"))   // Sinon, l'élément est soit une énumération, soit une interface
+        {
+            res.append(stereotype+" "+classe.getSimpleName().toString() + " <<"+stereotype+">>{\n");
+        }
+        else    // Si c'est une enumeration
         {
             res.append(stereotype+" "+classe.getSimpleName().toString() + " <<"+stereotype+">>{\n");
         }
 
+        //VariableElement v;
         return res;
     }
+
     public StringBuilder ajouteChamps(StringBuilder codePumlDeBase, Element classe)
     {
         StringBuilder res = new StringBuilder(codePumlDeBase);
@@ -178,6 +223,98 @@ public class PumlDiagram
         return res;
     }
 
+    public String getSuperClassName(Element classe)
+    {
+        TypeElement classeType = (TypeElement)classe;   // Récupération du type de la super classe
+        TypeMirror superClassType = classeType.getSuperclass();
 
+        String superSimpleClassName = ((DeclaredType) superClassType).asElement().getSimpleName().toString();
+
+        return superSimpleClassName;
+    }
+
+    public List<String> getInterfaces(Element classe)
+    {
+        List<String> res = new ArrayList<>();
+        List<? extends TypeMirror> types;
+
+        TypeElement classeType = (TypeElement)classe;
+        types = classeType.getInterfaces();
+
+        for( TypeMirror type : types )
+        {
+            res.add( ((DeclaredType)type).asElement().getSimpleName().toString() );
+        }
+
+        return res;
+    }
+
+    public StringBuilder ajouteAgregations(StringBuilder codePumlDeBase, Element classe)
+    {
+        StringBuilder res = new StringBuilder(codePumlDeBase);
+
+        List<String> agregationsExistantes = new ArrayList<>();   // Nous stockons toutes les agrégations dans cette liste afin de ne pas avoir de répétitions (par exemple : Boisson -- Substantif et Substantif -- Boisson)
+        List<String> typeNonVoulu = Arrays.asList("String", "Set", "List");     // Les String, Set et List ne sont pas considérés comme des types primitifs, mais nous ne les voulons pas dans l'UML
+
+        // Parcourt des champs de la classe
+        for( Element champOuConstructeurOuMethode : classe.getEnclosedElements() )
+        {
+            // Si l'élément est un champ ET ne possède pas un type primitif
+            if( champOuConstructeurOuMethode.getKind().equals(ElementKind.FIELD)  )
+            {
+                TypeMirror typeChamp = champOuConstructeurOuMethode.asType();
+                if( !typeChamp.getKind().isPrimitive() )
+                {
+                    // Nous récupérons le type
+                    String typeChampString = ((DeclaredType)typeChamp).asElement().getSimpleName().toString();
+                    if( !typeNonVoulu.contains(typeChampString)  )
+                    {
+                        String liaison = classe.getSimpleName()+" -- "+ typeChampString;
+
+                        // Nous ajoutons la liaison uniquement si celle-ci n'est pas présente (sinon il y aura des doublons)
+                        if( !liaisonExistante(agregationsExistantes, liaison) )
+                        {
+                            res.append(  liaison + "\n" );      // Ajout de la liaison dans le code
+                            agregationsExistantes.add(liaison);   // Ajout de la liaison à la liste pour ne pas avoir de doublon(s)
+                        }
+                    }
+                }
+            }
+        }
+
+        return res;
+    }
+
+    public boolean liaisonExistante(List<String> agregationsExistantes, String liaison)
+    {
+        boolean res = false;
+
+        String liaisonInverse = inverseLiaison(liaison);
+        if( agregationsExistantes.contains(liaison) || agregationsExistantes.contains(liaisonInverse) )
+        {
+            res = true;
+        }
+
+        return res;
+    }
+
+    public String inverseLiaison(String liaison)
+    {
+        String[] nomsClasses = liaison.split(" -- ");       // Récupération des noms de classes dans un tableau
+        StringBuilder laisonInverse = new StringBuilder();
+
+        laisonInverse.append(nomsClasses[1] + " -- " + nomsClasses[0]);     // Inversement
+
+        return laisonInverse.toString();
+    }
+
+    public StringBuilder ajoutePackage(StringBuilder codePumlDeBase, List<Element> classes)
+    {
+        String nomPackage = classes.get(0).getEnclosingElement().getSimpleName().toString();    // Récupération du package (étant donné que nous étudions uniquement
+
+        StringBuilder res = new StringBuilder(codePumlDeBase);
+        res.append("package "+nomPackage+"{\n\t");
+        return res;
+    }
 
 }
