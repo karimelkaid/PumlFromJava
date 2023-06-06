63b8aa13d79c178a04df710944d2d524c212781e
kelkaid <elkarim741@gmail.com>
2023-06-06 11:06:33 +0200

    En train d'avancer

--- src/pumlFromJava/Java2Puml.java
+++ src/pumlFromJava/Java2Puml.java
@@ -3,13 +3,17 @@ package pumlFromJava;
 
 import java.util.spi.ToolProvider;
 
+/*
+    - Ne pas présenter le code dès le début
+    - Voir le doc word
+*/
 
 public class Java2Puml
 {
 
     public static void main(String[] args)
     {
-        String[] arguments = {"-private","-sourcepath", "./src","-doclet" ,"pumlFromJava.PumlDoclet","-docletpath" ,"","western", "-d", "uml", "--dca" } ;
+        String[] arguments = {"-private","-sourcepath", "./src","-doclet" ,"pumlFromJava.PumlDoclet","-docletpath" ,"","western", "-d", "uml" } ;
 
         ToolProvider toolProvider = ToolProvider.findFirst("javadoc").get();
         System.out.println(toolProvider.name());
---
+++ src/test/Java2PumlTest1.java
@@ -0,0 +1,16 @@
+package test;
+
+import test.A;
+
+import java.util.*;
+
+/**
+ * traitement des types primitifs UMl et des multiplicités
+ */
+
+
+public class Java2PumlTest1
+{
+}
+
+
